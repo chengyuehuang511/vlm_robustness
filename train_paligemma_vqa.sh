ad17a4f09814ddf89df350ae593387548ce8e292
chengyuehuang511 <chuang475@gatech.edu>
2024-08-04 14:14:38 -0400

    sbatch cmd for different partitions

--- train_paligemma_vqa.sh
+++ train_paligemma_vqa.sh
@@ -2,9 +2,10 @@
 
 #SBATCH --partition="overcap"
 #SBATCH --nodes=1
+#SBATCH --cpus-per-gpu=6
 #SBATCH --gpus-per-node="a40:8"
 #SBATCH --qos="short"
-#SBATCH -x shakey,nestor,voltron,chappie,puma,randotron,cheetah,baymax,tachikoma,uniblab,major,optimistprime,hk47,xaea-12,dave,crushinator,kitt,gundam,megazord
+#SBATCH -x shakey,nestor,voltron,chappie,puma,randotron,cheetah,baymax,tachikoma,uniblab,optimistprime,hk47,xaea-12,dave,kitt,gundam,megazord,perseverance
 #SBATCH --mem-per-gpu=45G
 
 cd /nethome/chuang475/flash/projects/vlm_robustness/
