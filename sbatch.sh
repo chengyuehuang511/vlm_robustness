67af6425182629cd3864229cc125c98443b35716
chengyuehuang511 <chuang475@gatech.edu>
2024-10-13 16:06:18 -0400

    random updates

--- sbatch.sh
+++ sbatch.sh
@@ -5,6 +5,6 @@ name="train_paligemma_domainnet-real"
 # name="train_paligemma_imagenet1k"
 
 job_name="${name}_$(date +%Y%m%d_%H%M%S)"
-output_dir="output/domainnet/tpcgrad_1e-3_1e-4/${job_name}"
+output_dir="output/domainnet/adamp_1e-3_1e-4_k0/${job_name}"
 mkdir -p "$output_dir"
 sbatch --export "ALL" --job-name="${job_name}" --output="${output_dir}/slurm-%j.out" --error="${output_dir}/slurm-%j.err" run_scripts/train/${name}.sh
