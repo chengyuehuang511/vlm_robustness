26b6353abde3eb1d27ce56f7d4d220d35732a164
Meilong Zhang <meilongz@gatech.edu>
2025-01-27 00:08:17 -0500

    gqa_val run

--- run_scripts/test/paligemma/val_paligemma_gqa.sh
+++ run_scripts/test/paligemma/val_paligemma_gqa.sh
@@ -2,7 +2,7 @@
 #SBATCH -A gts-smukhopadhyay6-cognisense
 #SBATCH --cpus-per-gpu=8
 ##SBATCH --nodes=1
-#SBATCH -N1 --gres=gpu:h200:4
+#SBATCH -N1 --gres=gpu:a100:2
 ##SBATCH --constraint=A100-80GB
 ##SBATCH --mem-per-gpu=80G
 
@@ -22,4 +22,4 @@ export CUBLAS_WORKSPACE_CONFIG=:4096:8
 export PYTHONHASHSEED=42
 export TOKENIZERS_PARALLELISM=false
 
-srun -u python -m torch.distributed.run --nnodes=1 --nproc_per_node=4 --master_port=25678 evaluate.py --cfg-path configs/paligemma/gqa_val.yaml --options run.init_lr=$init_lr run.min_lr=$min_lr run.warmup_lr=$warmup_lr run.weight_decay=$weight_decay
+srun -u python -m torch.distributed.run --nnodes=1 --nproc_per_node=2 --master_port=25678 evaluate.py --cfg-path configs/paligemma/gqa_val.yaml --options run.init_lr=$init_lr run.min_lr=$min_lr run.warmup_lr=$warmup_lr run.weight_decay=$weight_decay
